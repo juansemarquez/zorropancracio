9ebed3a8e1a5955a1697f4d299adbb5bc14e3c04
juansemarquez <juansemarquez@yahoo.com.ar>
2022-02-05 23:29:40 -0300

    gitignore y archivo de credenciales de ejemplo

---
+++ credenciales.ejemplo.php
@@ -0,0 +1,12 @@
+<?php
+// Renombrar este archivo como credenciales.php y cargar las credenciales
+// correspondientes:
+function credenciales()
+{
+    return [
+        'server'=>"SERVIDOR",
+        'user'=>"USUARIO",
+        'pass'=>"CLAVE",
+        'db'=>"BASE_DE_DATOS",
+    ];
+}
